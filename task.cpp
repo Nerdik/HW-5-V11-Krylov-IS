fa15184450100a696c9bc6624fe490d6ae9192a2
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-15 18:57:10 +0300

    exercise 1 is completed

--- task.cpp
+++ task.cpp
@@ -4,32 +4,87 @@
 #include <cmath>
 
 
-int getFactorial(int x)
-{
-    unsigned long long result = 1;
+// int getFactorial(int x)
+// {
+//     unsigned long long result = 1;
 
-    for (int i = 1; i <= x; ++i)
-    {
-        result *= i;
-    }
+//     for (int i = 1; i <= x; ++i)
+//     {
+//         result *= i;
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 
-int main()
+// int main()
+// {
+//     int n;
+//     std::cout << "Input n: ";
+//     std::cin >> n;
+
+//     double sum = 0;
+
+//     for (int i = 1; i <= n; ++i)
+//     {
+//         sum += getFactorial(i);
+//     }
+
+
+
+//     return 0;
+// }
+
+
+// Function for Exercise 1.
+void ex1()
 {
     int n;
     std::cout << "Input n: ";
     std::cin >> n;
 
-    double sum = 0;
+    long long sum;
 
-    for (int i = 1; i <= n; ++i)
+    if (n == 0)
     {
-        sum += getFactorial(i);
+        sum = 1;
     }
+    else
+    {
+        sum = 0;
+        long long fact = 1;
 
+        for (int i = 1; i <= n; ++i)
+        {   
+            sum += fact;
+            fact *= (- 1) * (i + 1);
+        }
+    }
+     
+    std::cout << sum << std::endl;
+}
+
+// Function for Exercise 2.
+
+
+// Function for Exercise 3.
+
+
+// Function for Exercise 4.
+
+
+int main()
+{
+    // Exercise 1.
+    ex1();
+
+    // Exercise 2.
+
+
+    // Exercise 3.
+
+
+    // Exercise 4.
 
 
     return 0;
