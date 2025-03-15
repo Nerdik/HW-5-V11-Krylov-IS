13602361eba20b07ad788075d5edaae6d2f275e1
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-15 09:43:08 +0300

    added factorial function

--- task.cpp
+++ task.cpp
@@ -1,1 +1,36 @@
 // Variant 11 
+
+#include <iostream>
+#include <cmath>
+
+
+int getFactorial(int x)
+{
+    unsigned long long result = 1;
+
+    for (int i = 1; i <= x; ++i)
+    {
+        result *= i;
+    }
+
+    return result;
+}
+
+
+int main()
+{
+    int n;
+    std::cout << "Input n: ";
+    std::cin >> n;
+
+    double sum = 0;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += getFactorial(i);
+    }
+
+
+
+    return 0;
+}
