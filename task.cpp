a26ca7604980471bbcb164d8c38667731f310823
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-15 19:14:44 +0300

    exercise 2 is completed

--- task.cpp
+++ task.cpp
@@ -4,38 +4,6 @@
 #include <cmath>
 
 
-// int getFactorial(int x)
-// {
-//     unsigned long long result = 1;
-
-//     for (int i = 1; i <= x; ++i)
-//     {
-//         result *= i;
-//     }
-
-//     return result;
-// }
-
-
-// int main()
-// {
-//     int n;
-//     std::cout << "Input n: ";
-//     std::cin >> n;
-
-//     double sum = 0;
-
-//     for (int i = 1; i <= n; ++i)
-//     {
-//         sum += getFactorial(i);
-//     }
-
-
-
-//     return 0;
-// }
-
-
 // Function for Exercise 1.
 void ex1()
 {
@@ -60,12 +28,28 @@ void ex1()
             fact *= (- 1) * (i + 1);
         }
     }
-     
+
     std::cout << sum << std::endl;
 }
 
 // Function for Exercise 2.
+void ex2()
+{
+    int n, x;
+    std::cout << "Input n: ";
+    std::cin >> n;
+    std::cout << "Input x: ";
+    std::cin >> x;
 
+    double p = 1.0;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        double part = 1 + pow(x, i) / pow(i, 2);
+        p *= part;
+    }
+    std::cout << p;
+}
 
 // Function for Exercise 3.
 
@@ -76,10 +60,10 @@ void ex1()
 int main()
 {
     // Exercise 1.
-    ex1();
+    // ex1();
 
     // Exercise 2.
-
+    ex2();
 
     // Exercise 3.
 
